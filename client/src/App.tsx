160c1b2d9dd119f657ec408bc8ee260bc891dbc0
Daniel Benner (ext.) <Daniel.Benner@consol.de>
2023-06-22 10:55:26 +0200

    (feature) moved navbar and login up to app level

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,10 @@ import './App.css'
 
 import Errorpage from "./routes/Errorpage/Errorpage";
 import Home from "./routes/Home/Home";
+import Navbar from './Navbar/Navbar'
+import jwtDecode from "jwt-decode"
+import secrets from '../secrets.json'
+import { UserObject } from './interfaces';
 
 const router = createBrowserRouter([
   {
@@ -16,12 +20,37 @@ const router = createBrowserRouter([
 
 function App() {
 
+    const [user, setUser] = useState<UserObject>()
 
-  return (
-    <div className='app'>
-      <RouterProvider router={router} />
-    </div>
-  )
+    useEffect(() => {
+        /* global google */
+        google.accounts.id.initialize({
+            client_id: secrets.googleOAuth.client_id,
+            callback: handleCallbackResponse
+        })
+        google.accounts.id.prompt()
+        google.accounts.id.renderButton(
+            document.getElementById("googleSignIn"),
+            { theme: "filled_black", shape: ""}
+        )
+    }, [])
+
+    function handleCallbackResponse(response: any) {
+        const userObject: UserObject = jwtDecode(response.credential)
+        // console.log(userObject)
+        setUser(userObject)
+    }
+
+    function handleSignOut() {
+        setUser(undefined)
+    }
+
+    return (
+        <div className='app'>
+            <Navbar user={user} handleSignOut={() => handleSignOut()}/>
+            <RouterProvider router={router} />
+        </div>
+    )
 }
 
 export default App
--- client/src/routes/Home/Home.tsx
+++ client/src/routes/Home/Home.tsx
@@ -1,44 +1,19 @@
 import { useEffect, useState } from 'react'
-import Tree, { Quest, UserObject } from '../../interfaces'
+import Tree, { Quest } from '../../interfaces'
 import getQuestTrees from '../../api'
 import './Home.css'
 import Flow from './Flow/Flow'
 import Sidebar from './Sidebar/Sidebar'
-import Navbar from '../../Navbar/Navbar'
-import jwtDecode from "jwt-decode"
-import secrets from '../../../secrets.json'
 
 export default function Home() {
     const [questTrees, setQuestTrees] = useState<Tree[]>()
     const [questSelected, setQuestSelected] = useState<Quest>()
     const [sidebarDisplayed, setSidebarDisplayed] = useState(false)
     
-    const [user, setUser] = useState<UserObject>()
 
     useEffect(() => {
         setQuestTrees(getQuestTrees())
-        /* global google */
-        google.accounts.id.initialize({
-            client_id: secrets.googleOAuth.client_id,
-            callback: handleCallbackResponse
-        })
-        google.accounts.id.prompt()
-        google.accounts.id.renderButton(
-            document.getElementById("googleSignIn"),
-            { theme: "filled_black", shape: ""}
-        )
     }, [])
-  
-      function handleCallbackResponse(response: any) {
-          const userObject: UserObject = jwtDecode(response.credential)
-          // console.log(userObject)
-          setUser(userObject)
-      }
-  
-      function handleSignOut() {
-          setUser(undefined)
-      }
-    
 
     function setQuestIdSelected(quest: Quest | undefined) {
         setQuestSelected(quest)
@@ -52,7 +27,6 @@ export default function Home() {
 
     return (
         <>
-            <Navbar user={user} handleSignOut={() => handleSignOut()}/>
             <Sidebar visibility={sidebarDisplayed==true? 'visible': 'hidden'} questSelected={questSelected}/>
             {questTrees?
                 <Flow 
