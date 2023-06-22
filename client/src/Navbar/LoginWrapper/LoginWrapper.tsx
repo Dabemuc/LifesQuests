856cc251d0fa07c4bdf9c659bd05bb814f231bf2
Daniel Benner (ext.) <Daniel.Benner@consol.de>
2023-06-22 11:25:21 +0200

    (feature) cleaned up navbar

--- client/src/Navbar/LoginWrapper/LoginWrapper.tsx
+++ client/src/Navbar/LoginWrapper/LoginWrapper.tsx
@@ -12,12 +12,10 @@ export default function loginWrapper(props: Props) {
             <div id="googleSignIn" hidden={props.user? true: false}></div>
             { props.user &&
                 <div>
-                    <li className="nav-item dropdown">
-                        <img src={props.user.picture} className="dropdown-toggle profile-pic" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"/>
-                        <div className="dropdown-menu sign-out-btn" aria-labelledby="profile dropdown">
-                            <button className="dropdown-item" onClick={() => props.handleSignOut()}>Sign out</button>
-                        </div>
-                    </li>
+                    <img src={props.user.picture} className="dropdown-toggle profile-pic" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"/>
+                    <div className="dropdown-menu sign-out-btn" aria-labelledby="profile dropdown">
+                        <button className="dropdown-item" onClick={() => props.handleSignOut()}>Sign out</button>
+                    </div>
                 </div>
             }
         </div>
